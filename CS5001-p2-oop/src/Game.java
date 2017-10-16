e0ffdea13c028dc64198e643ab8a16f0de52aeaf
Fangyi Qi <32374583+FangyiQi@users.noreply.github.com>
2017-10-16 23:14:36 +0100

    Add files via upload

---
+++ CS5001-p2-oop/src/Game.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Game {
+
+   ArrayList <Enemy> enemies = new ArrayList <Enemy> ();
+   ArrayList <Tower> towers = new ArrayList <Tower> ();
+   Game(int corridorLength){
+   }
+   public void advance(){
+   }
+}
